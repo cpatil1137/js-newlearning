42571e1c97865c4f05d86306d34df9adfaf61336
cpatil1137 <162492456+cpatil1137@users.noreply.github.com>
2025-08-14 06:30:49 +0000

    datatypes

---
+++ 1_basicss/datatypes.js
@@ -0,0 +1,19 @@
+" use strict";
+console.log("Chetna");
+
+let age = 20
+let isLoggedIn = false
+let state
+//number => 2^53
+//bigint => for very very large numbers
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol =>  unique
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
