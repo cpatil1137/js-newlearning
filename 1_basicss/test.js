eff44e4a7ca4dbc937def886cde94fabc68eb6e5
cpatil1137 <162492456+cpatil1137@users.noreply.github.com>
2025-08-14 06:19:25 +0000

    variable

--- 1_basicss/test.js
+++ 1_basicss/test.js
@@ -1,1 +1,12 @@
-console.log("Chetna")
+const accountId=122233
+let accountEmail="cp9968302@gmail.com"
+var accountPassword="1367389"
+accountCity="Jaipur"
+let accountState
+console.log(accountId);
+accountEmail="chetnapatil1137@gmail.com"
+accountPassword="367547"
+acoountCity="Pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
